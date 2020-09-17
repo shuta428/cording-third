db0190a22e2b65b0b829cd13f9b6b6b08e7d746a
shuta428 <shuhamu428@gmail.com>
2020-09-18 00:04:38 +0900

    commit

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -32,4 +32,70 @@ $(function(){
     }
   });
 
+  var imgHeight = $('.abouts-kv-wrapper').outerHeight();
+  //kvの高さを取得
+  var bgHeight = $('.abouts-fv').outerHeight();
+  //fvの高さを取得
+  $(".burger-btn").on('click',function(){
+    if($(window).scrollTop() < imgHeight -50){
+      //ハンバーガーボタンがfvより上にある時
+      $('.bar').toggleClass('cross');
+      $('.header-nav').toggleClass('open');
+      $('burger-mask').toggleClass(300);
+      $('body').toggleClass('noscroll');
+    }else{
+      $(this).toggleClass('black');
+      $('.bar').toggleClass('cross');
+      $('.header-nav').toggleClass('open');
+      $('burger-mask').toggleClass(300);
+      $('body').toggleClass('noscroll');
+    }
+  });
+//スクロールしたら、サイドロゴとハンバーガーボタンの色が変わる
+  $(window).on('load scroll', function(){
+    if ($(window).scrollTop() < imgHeight -50) {
+      $('.site-title').removeClass('black');
+    }else {
+      $('.site-title').addClass('black');
+    }
+    if ($(window).scrollTop() < bgHeight -50) {
+      $('.burger-btn').removeClass('black');
+    }else {
+      $('.burger-btn').addClass('black');
+    }
+  });
+
+  var imgHeight = $('.contacts-kv-wrapper').outerHeight();
+  //kvの高さを取得
+  var bgHeight = $('.contacts-fv').outerHeight();
+  //fvの高さを取得
+  $(".burger-btn").on('click',function(){
+    if($(window).scrollTop() < imgHeight -50){
+      //ハンバーガーボタンがfvより上にある時
+      $('.bar').toggleClass('cross');
+      $('.header-nav').toggleClass('open');
+      $('burger-mask').toggleClass(300);
+      $('body').toggleClass('noscroll');
+    }else{
+      $(this).toggleClass('black');
+      $('.bar').toggleClass('cross');
+      $('.header-nav').toggleClass('open');
+      $('burger-mask').toggleClass(300);
+      $('body').toggleClass('noscroll');
+    }
+  });
+//スクロールしたら、サイドロゴとハンバーガーボタンの色が変わる
+  $(window).on('load scroll', function(){
+    if ($(window).scrollTop() < imgHeight -50) {
+      $('.site-title').removeClass('black');
+    }else {
+      $('.site-title').addClass('black');
+    }
+    if ($(window).scrollTop() < bgHeight -50) {
+      $('.burger-btn').removeClass('black');
+    }else {
+      $('.burger-btn').addClass('black');
+    }
+  });
+
 });
